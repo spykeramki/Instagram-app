c9f15c76395153b458814e2351002f8c55640d8c
spykeramki <ramakrishna.oguri@gmail.com>
2021-06-29 17:55:40 +0530

    Added more posts of friend for specific post

--- src/components/ProfilePage/ProfilePostComponent/index.js
+++ src/components/ProfilePage/ProfilePostComponent/index.js
@@ -1,17 +1,22 @@
+import {Link} from 'react-router-dom'
+
 import './index.css'
 
 const ProfilePostComponent = (props) => {
     const {profilePost} = props
-    const {postImageUrl} = profilePost
+    const {postImageUrl, postId} = profilePost
     return(
+        
         <li className="profile-page-post-item">
-            <div className="profile-page-post" 
-                style={{backgroundImage: `url(${postImageUrl})`, 
-                backgroundSize: "cover",
-                height: "100%",
-                backgroundPosition:'center'}}>
-                
-            </div>
+            <Link to={`/posts/${postId}`}>
+                <div className="profile-page-post" 
+                    style={{backgroundImage: `url(${postImageUrl})`, 
+                    backgroundSize: "cover",
+                    height: "100%",
+                    backgroundPosition:'center'}}>
+                    
+                </div>
+            </Link>
         </li>
     )
 }
--- src/components/SpecifiedUserPostDetailedPage/index.js
+++ src/components/SpecifiedUserPostDetailedPage/index.js
@@ -7,36 +7,13 @@ import SpecificUserPost from '../OtherUserPost/SpecificUserPost'
 
 import './index.css'
 
-let friendedPost = {
-    id: 0,
-    friendProfileImage: "https://res.cloudinary.com/dwlftsdge/image/upload/v1622897468/Instagram%20App/Nature%20images/profile%20images/5366575_jrfppm.jpg",
-    friendName: "ramaraju",
-    postContent: "https://res.cloudinary.com/dwlftsdge/image/upload/v1620653958/Instagram%20App/Nature%20images/post-image_trxquf.jpg",
-    friendPostTime: "2021-06-04 00:17:24",
-    user: "krishna",
-    likes: 4
-  }
-const postsInfo = [
-    {
-        postId: 21,
-        postType: "image",
-        postImageUrl: "https://firebasestorage.googleapis.com/v0/b/project-hamgram.appspot.com/o/userPosts%2F307779-P800TT-551.jpg?alt=media&token=71909ba8-c1cf-4984-ae6a-bd6260046fcb",
-        postCreatedTime: "2021-06-16 23:06:10"
-      },
-      {
-        postId: 22,
-        postType: "image",
-        postImageUrl: "https://firebasestorage.googleapis.com/v0/b/project-hamgram.appspot.com/o/userPosts%2F307779-P800TT-551.jpg?alt=media&token=71909ba8-c1cf-4984-ae6a-bd6260046fcb",
-        postCreatedTime: "2021-06-16 23:06:10"
-      }
-]
-
 class SpecifiedUserPostDetailedPage extends Component{
 
-    state={friendPost:{}, loading:true}
+    state={friendPost:{}, loading:true, otherPostsList:[], friendPetName:''}
 
     componentDidMount(){
         this.getPostDetails()
+        this.getOtherPostsDetails()
     }
 
     getPostDetails = async () => {
@@ -59,18 +36,47 @@ class SpecifiedUserPostDetailedPage extends Component{
                 id: dataFetched.data.id,
                 friendProfileImage: dataFetched.data.friend_profile_image,
                 friendName: dataFetched.data.friend_name,
+                petName: dataFetched.data.pet_name,
                 postContent: dataFetched.data.post_content,
                 likes: dataFetched.data.likes,
                 friendPostTime: dataFetched.data.friend_post_time,
                 user:dataFetched.data.user
             }
             console.log(updatedData)
-            this.setState({friendPost: updatedData, loading:false})
+            this.setState({friendPost: updatedData, loading:false, friendPetName:updatedData.petName})
+        }
+    }
+
+    getOtherPostsDetails = async () => {
+        const{match} = this.props
+         const{params} = match
+         const{id} = params
+        const jwtToken = Cookies.get('jwt_token');
+        const options = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json',
+                authorization: `Bearer ${jwtToken}`,
+            },
+        }
+        const response = await fetch(`http://localhost:3005/posts/${id}/other?limit=6`, options)
+        if (response.ok===true){
+            const fetchedData = await response.json()
+            const postsDetails = fetchedData.data.map(eachItem => {
+                return {
+                    postId: eachItem.post_id,
+                    postType: eachItem.post_type,
+                    postImageUrl: eachItem.post_url,
+                    postCreatedTime: eachItem.post_created_time
+                }
+            })
+            this.setState({otherPostsList:postsDetails})
         }
     }
 
     render(){
-        const {friendPost, loading} = this.state
+        const {friendPost, loading, otherPostsList, friendPetName} = this.state
         console.log(friendPost)
         return(
             <>
@@ -79,11 +85,11 @@ class SpecifiedUserPostDetailedPage extends Component{
                 {loading ? '' : <SpecificUserPost friendPost={friendPost}/>}
                 <hr className="specified-user-post-detailed-page-horizontal-line" />
                 <div className="specified-user-post-detailed-page-posts-list-heading-container">
-                    <p className="specified-user-post-detailed-page-posts-list-heading">More posts from <span>spyke</span></p>
+                    <p className="specified-user-post-detailed-page-posts-list-heading">More posts from <span className="specified-user-pet-name">{friendPetName}</span></p>
                 </div>
                 <div className="specified-user-post-detailed-page-posts-list-container">
                     <ul className="specified-user-post-detailed-page-posts-container">
-                        {postsInfo.map(eachItem => {
+                        {otherPostsList.map(eachItem => {
                             return <ProfilePostComponent profilePost={eachItem} key={eachItem.postId} />
                         })}
                     </ul>
